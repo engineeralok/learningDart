0c608c3b5567f5c97f05246494c9f9f946d7a321
engineeralok <engineeralok07@gmail.com>
2022-02-01 18:43:19 +0530

    list.dart file added which includes list in dart

---
+++ bin/list.dart
@@ -0,0 +1,15 @@
+void main() {
+  // * List in Dart* //-------------------------------------------------------
+
+  var lst = [1, 2, 3, 4];
+  print(lst);
+  print(lst.runtimeType);
+
+  var lsto = [1, 2, 3, 'hello'];
+  print(lsto);
+  print(lsto.runtimeType);
+
+  var lstn = <int>[1, 2, 3];
+  print(lstn);
+  print(lstn.runtimeType);
+}
