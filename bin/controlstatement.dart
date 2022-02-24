955a8166818eb1e2b249365141b43684cd5a3510
engineeralok <engineeralok07@gmail.com>
2022-02-24 20:13:48 +0530

    controlstatement.dart contain if-else statements

---
+++ bin/controlstatement.dart
@@ -0,0 +1,36 @@
+void main() {
+  //Control Flow Statement in Dart----------------------------------------
+  //*if statement --> extute the code when the condition is true-------------------------------
+  // var isLogin = true;
+  // if (isLogin) {
+  //   print("Welcome chief");
+  // }
+
+  // var a = 20;
+  // if (a == 20) {
+  //   print("value of a is $a");
+  // }
+
+  //*if - else statement --> when we have two conditions
+  // bool is_Login = false;
+  // if (is_Login == false) {
+  //   print("Welcome chief");
+  // }
+
+  // var a = 20;
+  // if (a == 10) {
+  //   print('Value of a is $a');
+  // } else {
+  //   print("Invalid Value");
+  // }
+
+  //*if - else if - else - Statememt --> whe we have more than two conditions
+  var a = 50;
+  if (a == 10) {
+    print("Value of a is $a");
+  } else if (a == 20) {
+    print("Value is $a");
+  } else {
+    print("Invalid Value");
+  }
+}
